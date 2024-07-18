c10e4b2a9dc09f0c60e23ed1971e7659a0204ed1
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-07-18 05:17:53 -0700

    init

--- MasteringPhotoPickerInSwiftUI/ContentView.swift
+++ MasteringPhotoPickerInSwiftUI/ContentView.swift
@@ -5,17 +5,35 @@
 //  Created by test on 7/18/24.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var photo: Image?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            PhotosPicker("Select a photo", selection: $selectedItem, matching: .images)
+            
+            if let photo {
+                photo
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(.rect(cornerRadius: 20))
+                    .frame(width: 300, height: 300)
+                    .shadow(radius: 5)
+            }
         }
-        .padding()
+        .onChange(of: selectedItem) { _, _ in
+            Task {
+                if let loadedImage = try? await selectedItem?.loadTransferable(type: Image.self) {
+                    photo = loadedImage
+                } else {
+                    print("Failed to load image")
+                }
+            }
+        }
     }
 }
 
