e1f76e4155d4f70a41cb11d0af95e486ce576ec0
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-07-18 05:52:37 -0700

    with continius and orderd selected behaviour & max selection count of 3

--- MasteringPhotoPickerInSwiftUI/ContentView.swift
+++ MasteringPhotoPickerInSwiftUI/ContentView.swift
@@ -9,24 +9,34 @@ import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedItem: PhotosPickerItem?
-    @State private var photo: Image?
+    @State private var selectedItems: [PhotosPickerItem] = []
+    @State private var photos: [Image] = []
     
     var body: some View {
         VStack {
-            PhotosPicker("Select a photo", selection: $selectedItem, matching: .images)
-                .photosPickerStyle(.compact)
+            PhotosPicker("Select a photo",
+                         selection: $selectedItems,
+                         maxSelectionCount: 3,
+                         selectionBehavior: .continuousAndOrdered,
+                         matching: .images)
+                .photosPickerStyle(.inline)
                 .frame(height: 300)
             
             Spacer()
             
-            if let photo {
-                photo
-                    .resizable()
-                    .scaledToFit()
-                    .clipShape(.rect(cornerRadius: 20))
-                    .frame(width: 300, height: 300)
-                    .shadow(radius: 5)
+            if photos.count > 0 {
+                ScrollView(.horizontal) {
+                    LazyHStack {
+                        ForEach(0..<photos.count, id: \.self) { idx in
+                            photos[idx]
+                                .resizable()
+                                .scaledToFit()
+                                .clipShape(.rect(cornerRadius: 20))
+                                .frame(width: 300, height: 300)
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
             } else {
                 ContentUnavailableView("Get started by selecting a photo from above",  systemImage: "photo")
                     .frame(height: 300)
@@ -38,12 +48,16 @@ struct ContentView: View {
                     .frame(width: 300, height: 300)
             }
         }
-        .onChange(of: selectedItem) { _, _ in
-            Task {
-                if let loadedImageData  = try? await selectedItem?.loadTransferable(type: Data.self), let uiImage = UIImage(data: loadedImageData) {
-                    photo = Image(uiImage: uiImage)
-                } else {
-                    print("Failed to load image")
+        .onChange(of: selectedItems ) { _, newValues in
+            photos.removeAll()
+            
+            newValues.forEach { selectedItem in
+                Task {
+                    if let loadedImageData  = try? await selectedItem.loadTransferable(type: Data.self), let uiImage = UIImage(data: loadedImageData) {
+                        photos.append(Image(uiImage: uiImage))
+                    } else {
+                        print("Failed to load image")
+                    }
                 }
             }
         }
