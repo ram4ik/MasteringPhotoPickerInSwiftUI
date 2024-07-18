5b2f9e8bf5e403c6866ea9367ae773515552f398
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-07-18 05:05:26 -0700

    Initial Commit

---
+++ MasteringPhotoPickerInSwiftUI/MasteringPhotoPickerInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasteringPhotoPickerInSwiftUIApp.swift
+//  MasteringPhotoPickerInSwiftUI
+//
+//  Created by test on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MasteringPhotoPickerInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MasteringPhotoPickerInSwiftUITests/MasteringPhotoPickerInSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  MasteringPhotoPickerInSwiftUITests.swift
+//  MasteringPhotoPickerInSwiftUITests
+//
+//  Created by test on 7/18/24.
+//
+
+import Testing
+@testable import MasteringPhotoPickerInSwiftUI
+
+struct MasteringPhotoPickerInSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
